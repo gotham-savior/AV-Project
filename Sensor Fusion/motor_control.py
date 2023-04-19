2be1b9075ce06496124e9d963e1209dbe7d8d9d5
sree <45594648+gotham-savior@users.noreply.github.com>
2023-04-19 12:26:14 -0700

    Update motor_control.py

--- Sensor Fusion/motor_control.py
+++ Sensor Fusion/motor_control.py
@@ -35,23 +35,39 @@ GPIO.setup(FR_ENA, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(FR_IN1, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(FR_IN2, GPIO.OUT, initial=GPIO.LOW)
 
+#initialize the PWM's on Jetson nano
+fl_pwm = GPIO.PWM(FL_ENA,50)
+rl_pwm = GPIO.PWM(RL_ENA,50)
+rr_pwm = GPIO.PWM(RR_ENA,50)
+fr_pwm = GPIO.PWM(FR_ENA,50)
+fl_pwm.start(0)
+rl_pwm.start(0)
+rr_pwm.start(0)
+fr_pwm.start(0)
+rpm_delta = 5 #range = 0-10
+#rpm range = 0-50
+
 def stop():
 # Stop
-	GPIO.output(FL_ENA, GPIO.HIGH)
+	#GPIO.output(FL_ENA, GPIO.HIGH)
 	GPIO.output(FL_IN1, GPIO.LOW)
 	GPIO.output(FL_IN2, GPIO.LOW)
-	GPIO.output(RL_ENA, GPIO.HIGH)
+	#GPIO.output(RL_ENA, GPIO.HIGH)
 	GPIO.output(RL_IN1, GPIO.LOW)
 	GPIO.output(RL_IN2, GPIO.LOW)
-	GPIO.output(FR_ENA, GPIO.HIGH)
+	#GPIO.output(FR_ENA, GPIO.HIGH)
 	GPIO.output(FR_IN1, GPIO.LOW)
 	GPIO.output(FR_IN2, GPIO.LOW)
-	GPIO.output(RR_ENA, GPIO.HIGH)
+	#GPIO.output(RR_ENA, GPIO.HIGH)
 	GPIO.output(RR_IN1, GPIO.LOW)
 	GPIO.output(RR_IN2, GPIO.LOW)
 
-def forward():
+def forward(rpm=10):
 # Forward
+	fl_pwm.ChangeDutyCycle(rpm)
+	rl_pwm.ChangeDutyCycle(rpm)
+	rr_pwm.ChangeDutyCycle(rpm)
+	fr_pwm.ChangeDutyCycle(rpm)
 	GPIO.output(FL_IN1, GPIO.HIGH)
 	GPIO.output(FL_IN2, GPIO.LOW)
 	GPIO.output(RL_IN1, GPIO.HIGH)
@@ -60,8 +76,12 @@ def forward():
 	GPIO.output(FR_IN2, GPIO.LOW)
 	GPIO.output(RR_IN1, GPIO.HIGH)
 	GPIO.output(RR_IN2, GPIO.LOW)
-def back():
+def back(rpm=10):
 	# Backward
+	fl_pwm.ChangeDutyCycle(rpm)
+	rl_pwm.ChangeDutyCycle(rpm)
+	rr_pwm.ChangeDutyCycle(rpm)
+	fr_pwm.ChangeDutyCycle(rpm)
 	GPIO.output(FL_IN1, GPIO.LOW)
 	GPIO.output(FL_IN2, GPIO.HIGH)
 	GPIO.output(RL_IN1, GPIO.LOW)
@@ -71,8 +91,14 @@ def back():
 	GPIO.output(RR_IN1, GPIO.LOW)
 	GPIO.output(RR_IN2, GPIO.HIGH)
 
-def right():
+def right(rpm=10):
 	# turn right
+	left_rpm = (rpm+rpm_delta)
+	right_rpm = (rpm-rpm_delta)
+	fl_pwm.ChangeDutyCycle(left_rpm)
+	rl_pwm.ChangeDutyCycle(left_rpm)
+	rr_pwm.ChangeDutyCycle(right_rpm)
+	fr_pwm.ChangeDutyCycle(right_rpm)
 	GPIO.output(FL_IN1, GPIO.HIGH)
 	GPIO.output(FL_IN2, GPIO.LOW)
 	GPIO.output(RL_IN1, GPIO.HIGH)
@@ -81,8 +107,14 @@ def right():
 	GPIO.output(FR_IN2, GPIO.HIGH)
 	GPIO.output(RR_IN1, GPIO.LOW)
 	GPIO.output(RR_IN2, GPIO.HIGH)
-def left():
+def left(rpm=10):
 	# turn left
+	left_rpm = (rpm-rpm_delta)
+	right_rpm = (rpm+rpm_delta)
+	fl_pwm.ChangeDutyCycle(left_rpm)
+	rl_pwm.ChangeDutyCycle(left_rpm)
+	rr_pwm.ChangeDutyCycle(right_rpm)
+	fr_pwm.ChangeDutyCycle(right_rpm)
 	GPIO.output(FL_IN1, GPIO.LOW)
 	GPIO.output(FL_IN2, GPIO.HIGH)
 	GPIO.output(RL_IN1, GPIO.LOW)
@@ -95,13 +127,13 @@ def left():
 
 stop()
 time.sleep(1)
-forward()
+forward(50)
 time.sleep(1)
-back()
+back(50)
 time.sleep(3)
-right()
+right(50)
 time.sleep(3)
-left()
+left(50)
 time.sleep(3)
 stop()
 
