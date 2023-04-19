ce83e17c3650c457fca69d90a86d5d62e4f1b037
sree <45594648+gotham-savior@users.noreply.github.com>
2023-04-19 00:23:47 -0700

    Update motor_control.py

--- Sensor Fusion/motor_control.py
+++ Sensor Fusion/motor_control.py
@@ -1,82 +1,99 @@
-import Jetson.GPIO as GPIO          
-from time import sleep
+#!/usr/bin/env python
 
-in1 = 24  # connect to pin 18
-in2 = 23  # connect to pin 16
-en = 25   # connect to pin 22
-temp1=1
+import RPi.GPIO as GPIO
+import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
-p.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
-
-while(1):
-
-    x=raw_input()
-    
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
-         print("forward")
-         x='z'
-        else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         print("backward")
-         x='z'
+# Define the pin connections to jetson nano FL- FRONT LEFT, FR- FRONT RIGHT, RR- REAR RIGHT, RL- REAR LEFT
+FL_ENA = 11
+FL_IN1 = 13
+FL_IN2 = 15
+RL_ENA = 33
+RL_IN1 = 37
+RL_IN2 = 35
+FR_ENA = 18
+FR_IN1 = 16
+FR_IN2 = 12
+RR_ENA = 40
+RR_IN1 = 38
+RR_IN2 = 36
 
 
-    elif x=='s':
-        print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        x='z'
+# set pin numbers to the board's
+GPIO.setmode(GPIO.BOARD)
 
-    elif x=='f':
-        print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        temp1=1
-        x='z'
+# initialize FL_ENA, FL_IN1 and FL_IN2
+GPIO.setup(FL_ENA, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(FL_IN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(FL_IN2, GPIO.OUT, initial=GPIO.LOW)
 
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        temp1=0
-        x='z'
+def stop():
+# Stop
+	GPIO.output(FL_ENA, GPIO.HIGH)
+	GPIO.output(FL_IN1, GPIO.LOW)
+	GPIO.output(FL_IN2, GPIO.LOW)
+	GPIO.output(RL_ENA, GPIO.HIGH)
+	GPIO.output(RL_IN1, GPIO.LOW)
+	GPIO.output(RL_IN2, GPIO.LOW)
+	GPIO.output(FR_ENA, GPIO.HIGH)
+	GPIO.output(FR_IN1, GPIO.LOW)
+	GPIO.output(FR_IN2, GPIO.LOW)
+	GPIO.output(RR_ENA, GPIO.HIGH)
+	GPIO.output(RR_IN1, GPIO.LOW)
+	GPIO.output(RR_IN2, GPIO.LOW)
 
-    elif x=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        x='z'
+def forward():
+# Forward
+	GPIO.output(FL_IN1, GPIO.HIGH)
+	GPIO.output(FL_IN2, GPIO.LOW)
+	GPIO.output(RL_IN1, GPIO.HIGH)
+	GPIO.output(RL_IN2, GPIO.LOW)
+	GPIO.output(FR_IN1, GPIO.HIGH)
+	GPIO.output(FR_IN2, GPIO.LOW)
+	GPIO.output(RR_IN1, GPIO.HIGH)
+	GPIO.output(RR_IN2, GPIO.LOW)
+def back():
+	# Backward
+	GPIO.output(FL_IN1, GPIO.LOW)
+	GPIO.output(FL_IN2, GPIO.HIGH)
+	GPIO.output(RL_IN1, GPIO.LOW)
+	GPIO.output(RL_IN2, GPIO.HIGH)
+	GPIO.output(FR_IN1, GPIO.LOW)
+	GPIO.output(FR_IN2, GPIO.HIGH)
+	GPIO.output(RR_IN1, GPIO.LOW)
+	GPIO.output(RR_IN2, GPIO.HIGH)
 
-    elif x=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        x='z'
+def right():
+	# turn right
+	GPIO.output(FL_IN1, GPIO.HIGH)
+	GPIO.output(FL_IN2, GPIO.LOW)
+	GPIO.output(RL_IN1, GPIO.HIGH)
+	GPIO.output(RL_IN2, GPIO.LOW)
+	GPIO.output(FR_IN1, GPIO.LOW)
+	GPIO.output(FR_IN2, GPIO.HIGH)
+	GPIO.output(RR_IN1, GPIO.LOW)
+	GPIO.output(RR_IN2, GPIO.HIGH)
+def left():
+	# turn left
+	GPIO.output(FL_IN1, GPIO.LOW)
+	GPIO.output(FL_IN2, GPIO.HIGH)
+	GPIO.output(RL_IN1, GPIO.LOW)
+	GPIO.output(RL_IN2, GPIO.HIGH)
+	GPIO.output(FR_IN1, GPIO.HIGH)
+	GPIO.output(FR_IN2, GPIO.LOW)
+	GPIO.output(RR_IN1, GPIO.HIGH)
+	GPIO.output(RR_IN2, GPIO.LOW)
 
-    elif x=='h':
-        print("high")
-        p.ChangeDutyCycle(75)
-        x='z'
-     
-    
-    elif x=='e':
-        GPIO.cleanup()
-        break
-    
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+
+stop()
+time.sleep(1)
+forward()
+time.sleep(1)
+back()
+time.sleep(3)
+right()
+time.sleep(3)
+left()
+time.sleep(3)
+stop()
+
+GPIO.cleanup()
